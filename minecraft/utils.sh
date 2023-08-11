8735775d4197a38881e81578a800d4ab87844496
Chris L <chrlew082@gmail.com>
2023-08-10 20:01:48 -0600

    move utils.sh

---
+++ minecraft/utils.sh
@@ -0,0 +1,23 @@
+# tput
+
+export BLACK=$(tput setaf 0)
+export RED=$(tput setaf 1)
+export GREEN=$(tput setaf 2)
+export YELLOW=$(tput setaf 3)
+export BLUE=$(tput setaf 4)
+export MAGENTA=$(tput setaf 5)
+export CYAN=$(tput setaf 6)
+export WHITE=$(tput setaf 7)
+
+export RESET=$(tput sgr0)
+
+# properties editor
+getProperty {
+   PROP_KEY=$1
+   PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+   echo $PROP_VALUE
+}
+setProperty(){
+  awk -v pat="^$1=" -v value="$1=$2" '{ if ($0 ~ pat) print value; else print $0; }' $3 > $3.tmp
+  mv $3.tmp $3
+}
