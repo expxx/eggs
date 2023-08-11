ae15f7abc4adf633716db81444dcabd21c53f7a2
Chris L <chrlew082@gmail.com>
2023-08-10 20:42:27 -0600

    update utils

--- utils.sh
+++ utils.sh
@@ -12,12 +12,12 @@ export WHITE=$(tput setaf 7)
 export RESET=$(tput sgr0)
 
 # properties editor
-getProperty {
+function getProperty {
    PROP_KEY=$1
    PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
    echo $PROP_VALUE
 }
-setProperty(){
+function setProperty() {
   awk -v pat="^$1=" -v value="$1=$2" '{ if ($0 ~ pat) print value; else print $0; }' $3 > $3.tmp
   mv $3.tmp $3
 }
