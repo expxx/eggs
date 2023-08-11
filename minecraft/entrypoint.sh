980bba726fb8bd8fce4a2010b6b379465a2b12f0
ExerpimentalX <chrlew082@gmail.com>
2023-08-11 04:11:04 +0000

    Remove un-needed debug: mc-ent

--- minecraft/entrypoint.sh
+++ minecraft/entrypoint.sh
@@ -4,19 +4,14 @@ source /utils.sh || exit 403
 TZ=${TZ:-UTC}
 export TZ
 
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
 cd /home/container || exit 1
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
 PROPERTY_FILE=server.properties
 
-
-
 max_players=$(getProperty max-players)
 
-echo "${MAGENTA}[DEBUG] ${gray}${max_players}${reset}"
+#echo "${MAGENTA}[DEBUG] ${gray}${max_players}${reset}"
 
 if [[ -z ${max_players} ]];
 then
